ec061b071cafaad75003eb48ef7a263690a2eaa5
Sandro <rolosandro10@gmail.com>
2026-05-29 14:11:34 +0200

    Primera version del proyecto

---
+++ js/data/recetas.js
@@ -0,0 +1,7 @@
+export const recetas = [
+  {
+    id: 1,
+    nombre: "Tortilla de patatas",
+    descripcion: `Tortilla de patatas es un plato tradicional de la gastronomía española elaborado principalmente a base de huevos y patatas, cocinados juntos hasta formar una preparación compacta, de textura suave y consistente. Se trata de una elaboración muy representativa de la cocina casera en España, caracterizada por su sencillez de ingredientes y su versatilidad como plato principal o acompañamiento. Puede servirse en frío o en caliente, y se consume habitualmente tanto en el ámbito doméstico como en bares y restaurantes. Es uno de los platos más reconocidos de la cocina española a nivel internacional, valorado por su equilibrio entre sabor, textura y facilidad de conservación.`,
+  },
+];
